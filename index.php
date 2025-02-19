16237182d5046b937cc92f64aa39b0c3adaba486
Eugene van der Merwe <eugene@vander.host>
2025-02-19 13:36:55 +0200

    add Discord and clean up Hello Peter example

--- index.php
+++ index.php
@@ -5,37 +5,50 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Dotenv\Dotenv;
 use Eugenevdm\HelloPeter;
 use Eugenevdm\BulkSMS;
+use Eugenevdm\Discord;
 use Eugenevdm\Slack;
 use Eugenevdm\Telegram;
 use Eugenevdm\ZADomains;
+
 // Load environment variables
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-// Initialize the HelloPeter client
-$client = new HelloPeter($_ENV['HELLO_PETER_API_KEY']);
-
 try {
-    // Send SMS
+    // Send an SMS using the BulkSMS API
     if ($_ENV['ENABLE_BULKSMS'] === 'true') {
         $sender = new BulkSMS($_ENV['BULKSMS_USERNAME'], $_ENV['BULKSMS_PASSWORD']);
         $recipients = explode(',', $_ENV['BULKSMS_RECIPIENTS']);
         $sender->sendSMS($message, $recipients);
     }
 
-    // Send Slack notification
+    // Get information about a Discord user
+    if ($_ENV['ENABLE_DISCORD'] === 'true') {
+        $userId = $_ENV['DISCORD_USER_ID'];
+        $discord = new Discord();
+        $discord->getUser($userId);
+    }
+
+    // Get unreplied reviews from Hello Peter
+    if ($_ENV['ENABLE_HELLO_PETER'] === 'true') {
+        $client = new HelloPeter($_ENV['HELLO_PETER_API_KEY']);
+        $unrepliedReviews = $client->getUnrepliedReviews();
+        ray($unrepliedReviews);
+    }
+
+    // Send a Slack notification
     if ($_ENV['ENABLE_SLACK'] === 'true') {
         $slack = new Slack($_ENV['SLACK_WEBHOOK_URL']);
         $slack->sendMessage($message);
     }
 
-    // Send Telegram notification
+    // Send a Telegram notification
     if ($_ENV['ENABLE_TELEGRAM'] === 'true') {
         $telegram = new Telegram($_ENV['TELEGRAM_BOT_TOKEN'], $_ENV['TELEGRAM_CHAT_ID']);
         $telegram->sendMessage($message);    
     }
 
-    // Send ZADomains notification
+    // Retrieve ZADomains Domain Information
     if ($_ENV['ENABLE_ZADOMAINS'] === 'true') {
         $zadomains = new ZADomains($_ENV['ZADOMAINS_USERNAME'], $_ENV['ZADOMAINS_PASSWORD']);
         $result = $zadomains->getDomainInfo($_ENV['ZADOMAINS_TEST_DOMAIN']);
---
+++ src/Discord.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Eugenevdm;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+class Discord
+{
+    /**
+     * Get user information by user ID
+     *
+     * @param string $userId The Discord user ID to lookup
+     * @return array Returns user data including id, username, and avatar
+     * @throws GuzzleException
+     */
+    public static function getUser(string $userId)
+    {
+        header('Content-Type: application/json');
+
+        $botToken = $_ENV['DISCORD_BOT_TOKEN'];
+
+        $url = "https://discord.com/api/v10/users/{$userId}";
+
+        $client = new Client();
+
+        $response = $client->request('GET', $url, [
+            'headers' => [
+                'Authorization' => "Bot {$botToken}",
+                'Content-Type' => 'application/json',
+            ],
+        ]);
+
+        echo $response->getBody();
+    }
+}
