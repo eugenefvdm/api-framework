a7f75ecddf7070671c1a15180cb1281c18981260
Eugene van der Merwe <eugene@vander.host>
2025-03-24 12:20:17 +0200

    wip

--- src/Dns.php
+++ src/Dns.php
@@ -4,19 +4,34 @@ namespace Eugenefvdm\Api;
 
 class Dns
 {    
+    public function MX(string $domain, $preferNative = false)
+    {
+        if (!$preferNative) {
+            $results = shell_exec("dig +tries=2 +short MX $domain");
+
+            $lines = explode("\n", $results);
+    
+            unset($lines[count($lines) - 1]); // The rid of last newline outputted on the command line
+
+            return $lines;
+        }
+
+        return dns_get_record($domain, DNS_MX);
+    }
+
     public function NS(string $domain) : array
     {
         $records = dns_get_record($domain, DNS_NS);
-        $nameServers = [];
+        $servers = [];
         
         if ($records) {
             foreach ($records as $record) {
                 if (isset($record['target'])) {
-                    $nameServers[] = $record['target'];
+                    $servers[] = $record['target'];
                 }
             }
         }
 
-        return $nameServers;
+        return $servers;
     }
 }
